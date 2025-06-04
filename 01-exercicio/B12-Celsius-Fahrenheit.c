af65fd23c6e51f3a3c93c9105da49ce4bc254ec7
audreyrdsc <audreyrdsc@gmail.com>
2025-06-04 20:26:41 -0300

    Resolução exercício B12 - Conversão de Celsius para Fahreheit

---
+++ 01-exercicio/B12-Celsius-Fahrenheit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    float tf, tc;
+
+    printf("\nDigite a temperatura em Celsius: ");
+    scanf("%f", &tc);
+
+    tf = (9 * tc + 160) / 5;
+
+    printf("\n%.2f graus Celsius corresponde a %.2f graus Fahrenheit \n", tc, tf);
+
+    return 0;
+}
