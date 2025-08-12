45118593a8fad735c8668e9759c359fd7ad6c866
audreyrdsc <audreyrdsc@gmail.com>
2025-08-12 19:05:38 -0300

    Exercício resolvido C43

---
+++ Lista-IFCatarinense/C43-AumentoSalarioFuncionarios.c
@@ -0,0 +1,9 @@
+//43. Uma empresa irá dar um aumento de salário aos seus funcionários de acordo com a categoria de 
+//cada empregado. O aumento seguirá a seguinte regra:  
+//• Funcionários das categorias A, C, F, e H ganharão 10% de aumento sobre o salário; 
+//• Funcionários das categorias B, D, E, I, J e T ganharão 15% de aumento sobre o salário; 
+//• Funcionários das categorias K e R ganharão 25% de aumento sobre o salário;
+//• Funcionários das categorias L, M, N, O, P, Q e S ganharão 35% de aumento sobre o salário; 
+//• Funcionários das categorias U, V, X, Y, W e Z ganharão 50% de aumento sobre o salário. 
+//Faça um algoritmo que escreva nome, categoria e salário reajustado de cada empregado.
+
